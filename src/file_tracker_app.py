0814b680e4b537b0f6515b4f723d41b17aaa478c
CleveTok3125 <CleveTok3125@gmail.com>
2025-05-11 02:38:36 +0700

    Improved UI and implemented some features

--- src/file_tracker_app.py
+++ src/file_tracker_app.py
@@ -1,12 +1,13 @@
 import os
 from typing import List
 
+from textual import on
 from textual.app import App, ComposeResult
-from textual.widgets import Tree, Header, Footer, Static, Button
+from textual.widgets import Tree, Header, Footer, Static, Button, Input, Label
 from textual.containers import Container, Vertical, HorizontalGroup
 from textual.widgets.tree import TreeNode
 
-from file_tracker_core import ConfigFileHandler
+from file_tracker_core import ConfigFileHandler, Tracker
 
 
 class PathBeautify:
@@ -52,27 +53,72 @@ class Forests:
         Forests.populate_tree(tree.root, tree_data)
         return tree
 
+
+class MessageBar(Static):
+    def on_mount(self):
+        self.display = False
+
+    def show_message(self, message: str, duration: float = 3.0):
+        self.update(f"Message: {message}")
+        self.display = True
+        self.set_timer(duration, self.clear_message)
+
+    def clear_message(self):
+        self.update("")
+        self.display = False
+
+
 class UserConfig:
-    config_path = 'user_config.json'
+    root = "./"
+    config_path = "user_config.json"
+    auto_save = True
+    auto_clean = True
+
+
+class UserInput:
+    value = ""
+
+
+class CoreInstance:
+    config_file_handler: ConfigFileHandler = ConfigFileHandler(UserConfig.config_path)
+
+    @staticmethod
+    def init_tracker():
+        tracker = Tracker(
+            config=CoreInstance.config_file_handler.safe_read_config(),
+            config_path=UserConfig.config_path,
+            auto_save=UserConfig.auto_save,
+            auto_clean=UserConfig.auto_clean,
+        )
+        return tracker
+
 
 class CoreAPI:
     @staticmethod
     def get_files_tracked() -> List[str]:
-        config_file_handler: ConfigFileHandler = ConfigFileHandler(UserConfig.config_path)
-        files_tracked: List[str] = config_file_handler.get_files_tracked()
+        files_tracked: List[str] = CoreInstance.config_file_handler.get_files_tracked()
         files_tracked = PathBeautify.simplify(files_tracked)
         return files_tracked
 
 
 class TreeContainer(Container):
     def compose(self) -> ComposeResult:
-        paths = CoreAPI.get_files_tracked()
+        yield from self.build_tree()
 
+    def build_tree(self) -> List[str]:
+        paths = CoreAPI.get_files_tracked()
         if not paths:
-            yield Static("No files are tracked", id="no-files-messages")
-        else:
-            tree = Forests.plant_tree(paths)
-            yield tree
+            return [
+                Static(
+                    "No files are tracked", id="no-files-messages", classes="messages"
+                )
+            ]
+        tree = Forests.plant_tree(paths)
+        return [tree]
+
+    def refresh_tree(self):
+        self.remove_children()
+        self.mount_all(self.build_tree())
 
 
 class ActionButton(HorizontalGroup):
@@ -80,24 +126,64 @@ class ActionButton(HorizontalGroup):
         yield Button("Add File", id="add_file", classes="action-button")
         yield Button("Add Directory", id="add_dir", classes="action-button")
         yield Button("Remove File", id="remove_file", classes="action-button")
-        yield Button("Save File", id="save_file", classes="action-button")
+        yield Button("Change working dir", id="change_root", classes="action-button")
 
 
 class MainApp(App):
     CSS_PATH = "file_tracker_app.tcss"
-    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+    BINDINGS = [
+        ("q", "quit_app", "Quit"),
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("r", "refresh_tree", "Refresh tree"),
+        ("s", "save_conf", "Save configuration"),
+    ]
 
     def compose(self) -> ComposeResult:
         yield Header()
-
-        yield Vertical(TreeContainer(id="tree-container"), ActionButton())
-
+        yield Vertical(
+            Static(id="top-gap", classes="messages"),
+            TreeContainer(id="tree-container"),
+            MessageBar(id="message-bar", classes="messages"),
+            Input(
+                placeholder="Enter a path or regular expression here then select one of the buttons below",
+                id="input-bar",
+            ),
+            ActionButton(),
+        )
         yield Footer()
 
     def action_toggle_dark(self) -> None:
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
+
+    def _get_input(self) -> UserInput.value:
+        input = self.query_one(Input)
+        UserInput.value = input.value
+        input.value = ""
+
+    def _refresh_tree(self) -> None:
+        tree_container = self.query_one(TreeContainer)
+        tree_container.refresh_tree()
+
+    def _send_message(self, message: str, timeout: float = 3.0) -> None:
+        message_bar = self.query_one("#message-bar", MessageBar)
+        message_bar.show_message(message, timeout)
+
+    @on(Button.Pressed, "#add_file")
+    def button_add_file(self) -> None:
+        self._get_input()
+        tracker = CoreInstance.init_tracker()
+        tracker.add_file(UserInput.value, UserConfig.root)
+        self._refresh_tree()
+        self._send_message("File added successfully")
+
+    def action_refresh_tree(self) -> None:
+        self._refresh_tree()
+        self._send_message("Reloaded directory tree")
+
+    def action_quit_app(self) -> None:
+        self.exit()
 
 
 if __name__ == "__main__":
